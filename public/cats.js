eea23d29fbb0db55551b9b77a809a3726391020a
filipnest <filip@octophin.com>
2024-10-01 21:22:54 +0100

    Test

---
+++ public/cats.js
@@ -0,0 +1,7 @@
+const map = new mapboxgl.Map({
+  accessToken:
+    "pk.eyJ1Ijoib2N0b3BoaW4iLCJhIjoiY2s1MmtzMnFwMDllYjNtcW43bzZmZnpyNyJ9.w6pj26TwKy5wqaCQN69sFg",
+  container: "map",
+  center: [-0.10588288303432236, 51.518095113618116],
+  zoom: 14,
+});
